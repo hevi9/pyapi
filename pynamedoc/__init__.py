373af165dc8f4f87525cd671dc35796612dddca4
hevi <hevi@lut.fi>
2013-08-22 14:47:15 +0300

    added package

---
+++ pynamedoc/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+## -*- coding: utf-8 -*-
+## Copyright (C) 2013 Petri Heinilä, License LGPL 2.1
+""" """
+
+import logging
+log = logging.getLogger(__name__)
+D = log.debug
+
+def main():
+  pass
+
+if __name__ == "__main__": main()
