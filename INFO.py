915a8956c0d1963ff9ce91364bb690c03e9cbb83
hevi <hevi@lut.fi>
2013-08-22 14:50:55 +0300

    added info

---
+++ INFO.py
@@ -0,0 +1,9 @@
+# Project basic information
+# this is to be imported/included by python, sh, make
+
+name    = "pynamedoc"
+version = 0
+author  = "Petri Heinilä"
+url     = "http://github.com/hevi9/pynamedoc"
+license = "LGPL 2.1"
+
