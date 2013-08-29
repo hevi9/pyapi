b82b6dab9c876d40c7d5d051c041dff821691fb9
hevi <hevi@lut.fi>
2013-08-30 01:31:37 +0300

    extractor base working

--- pyapi/extractor.py
+++ pyapi/extractor.py
@@ -4,12 +4,18 @@
 """ Extract names from python module. """
 
 import logging
+from inspect import ismodule
 log = logging.getLogger(__name__)
 D = log.debug
 import os
 j = os.path.join
 from hevi_util.module import module_name, import_file
-import importlib as imp
+from hevi_util.tagtree import Forest, Entry
+import importlib as il
+import importlib.machinery as ilm 
+import sys
+from types import *
+import inspect
 
 def find_files(path, root = None):
   """ """
@@ -29,17 +35,77 @@ def find_files(path, root = None):
         yield i
 
 def filter_files(iter):
-  suffixes = imp.machinery.SOURCE_SUFFIXES + imp.machinery.EXTENSION_SUFFIXES
+  suffixes = ilm.SOURCE_SUFFIXES + ilm.EXTENSION_SUFFIXES
   for root, path in iter:
     if os.path.isdir(j(root,path)):
+      if os.path.basename(path) == "__pycache__": continue
       yield root,path
     if os.path.basename(path) == "__init__.py": continue
     if not os.path.splitext(path)[1] in suffixes: continue
     yield root,path
     
 def extract_module_file(root,path,forest):
+  D("extract_module_file({})".format(path))
+  ## insert root to sys.path
+  inserted = False
+  if root not in sys.path:
+    sys.path.insert(0, root)
+    inserted = True
+  ## import 
+  mname = module_name(path)
+  mpath = mname.split(".")
+  centry = forest
+  for i in range(0,len(mpath)):
+    if mpath[i] not in centry:
+      mname2 = ".".join(mpath[:i+1])
+      mobj = il.import_module(mname2)
+      centry = Entry(centry,mpath[i],mobj)
+      extract_module_to_entry(centry)
+    else:
+      centry = centry[mpath[i]]
+  ## clean up sys.path
+  if inserted: sys.path.remove(root)
   
+def extract_module_to_entry(entry):
+  assert type(entry.obj) is ModuleType
+  D("extract_module_to_entry({})".format(entry.path))
+  # extract data
+  # create members
+  for name,obj in inspect.getmembers(entry.obj):
+    if is_name_ignore(name): continue
+    subentry = Entry(entry,name,obj)
+    extract_thing(subentry)
+  # tags
+  entry.tags.add("module")
+  
+def extract_thing(entry):
+  D("extract_thing({})".format(entry.path))
+  assert entry.obj is not None
+  if is_composite(entry.obj):
+    for name,obj in inspect.getmembers(entry.obj):
+      if is_name_ignore(name): continue
+      subentry = Entry(entry,name,obj)
+      extract_thing(subentry)
 
+def is_composite(obj):
+  return inspect.ismodule(obj) or inspect.isclass(obj)
+    
+def is_name_ignore(name):
+  if is_system_name(name): return True
+  return False
+
+def is_system_name(name):
+  return name in (
+    "__builtins__",
+    "__cached__",
+    "__doc__",
+    "__file__",
+    "__initializing__",
+    "__loader__",
+    "__name__",
+    "__package__",
+    "__path__"
+  )
   
 ##############################################################################
 __doc__ += """
--- test/test_extractor.py
+++ test/test_extractor.py
@@ -7,47 +7,60 @@ import logging
 log = logging.getLogger(__name__)
 D = log.debug
 logging.basicConfig(level=logging.DEBUG)
-import pynamedoc.extractor as sut
+import pyapi.extractor as sut
 from hevi_util.processes import cd
 import os
 j = os.path.join
+import unittest
+import sys
+from hevi_util.tagtree import Forest, Entry, dump
 
 root = j(os.getcwd(),"testforest")
+if not os.path.isdir(root):
+  root = os.path.normpath(j(os.getcwd(),"..","testforest"))
+  if not os.path.isdir(root):
+    sys.exit(1)
 
-def test_01():
-  """ file found """
-  with cd("testforest"):
-    assert (root,"justamodule.py") in [i for i in sut.find_files("justamodule.py")]
+class test_extractor(unittest.TestCase):
 
-def test_02():
-  """ package tree found """
-  files = [i for i in sut.find_files(j(root,"package"))]
-  assert (root,"package") in files
-  assert (root,"package/module.py") in files
-  assert (root,"package/subpackage") in files
-  assert (root,"package/subpackage/module.py") in files
-
-def test_03():
-  """ package tree found no __init__ """
-  files = [i for i in sut.find_files(j(root,"packagenoinit"))]
-  assert (root,"packagenoinit") in files
-  assert (root,"packagenoinit/module.py") in files
-  assert (root,"packagenoinit/subpackage") in files
-  assert (root,"packagenoinit/subpackage/module.py") in files
-
-def test_04():
-  """ filter non-python files """
-  #files = [i for i in sut.find_files(j(root,"package"))]
-  files = [i for i in sut.filter_files(sut.find_files(j(root,"package")))]
-  #D(files)
-
-def test_05():
-  """  """
-  for r,path in sut.filter_files(sut.find_files(j(root,"package"))):
-    D(j(r,path))
+  def test_01(self):
+    """ file found """
+    with cd(root):
+      assert (root,"justamodule.py") in [i for i in sut.find_files("justamodule.py")]
   
+  def test_02(self):
+    """ package tree found """
+    files = [i for i in sut.find_files(j(root,"package"))]
+    assert (root,"package") in files
+    assert (root,"package/module.py") in files
+    assert (root,"package/subpackage") in files
+    assert (root,"package/subpackage/module.py") in files
   
+  def test_03(self):
+    """ package tree found no __init__ """
+    files = [i for i in sut.find_files(j(root,"packagenoinit"))]
+    assert (root,"packagenoinit") in files
+    assert (root,"packagenoinit/module.py") in files
+    assert (root,"packagenoinit/subpackage") in files
+    assert (root,"packagenoinit/subpackage/module.py") in files
   
-if __name__ == "__main__": test_02()
+  def test_04(self):
+    """ filter non-python files """
+    files = [i for i in sut.filter_files(sut.find_files(j(root,"package")))]
+    #D(files)
+    assert (root,"package/nonpython.txt") not in files
+    #assert (root,"package/module.py") not in files
+
+  def test_05(self):
+    """ extract modules into forest """
+    forest = Forest()
+    for r,p in sut.filter_files(sut.find_files(j(root,"package"))):
+      #D("{} {}".format(r,p))
+      sut.extract_module_file(r, p, forest)
+    #dump(forest)
+      
+if __name__ == "__main__": 
+  #test_02()
+  unittest.main()
   
   
